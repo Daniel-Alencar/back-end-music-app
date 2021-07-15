0ab318b38ef0b68fb88b4b742ce59ffc3694756c
Daniel-Alencar <danielalencar746@gmail.com>
2021-07-15 10:09:20 -0300

    feat:script created to add new users

--- src/database/controllers/UserController.ts
+++ src/database/controllers/UserController.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../../models/User';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const body = request.body;
-    console.log(body);
+    const { name, email } = request.body;
 
-    return response.send("Acabou");
+    const usersRepository = getRepository(User);
+    const someUser = await usersRepository.findOne({
+      email: email
+    });
+
+    if(someUser) {
+      return response.status(400).json({
+        error: "User already exists"
+      });
+    }
+
+    const user = usersRepository.create({
+      email: email,
+      name: name,
+    });
+    await usersRepository.save(user);
+
+    return response.status(201).json(user);
   }
 }
 
